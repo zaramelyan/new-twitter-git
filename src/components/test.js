a502741b74685f860b296ae605cbc5dd0b394395
zarmel <50460344+zaramelyan@users.noreply.github.com>
2020-09-11 19:10:06 +0200

    test

---
+++ src/components/test.js
@@ -0,0 +1,42 @@
+function solution(A, B, P) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    var contacts = [];
+
+   //check 
+   //[ '121212121', '111111111', '444555666' ]
+   function check(num) {
+       
+   }
+   //get ind
+   
+   //check errors
+}
+
+var contacts = [];
+
+for (var i = 0; i < ar.length; i++) {
+    if (ar.match) {
+        contacts.push(ar.indexOf())
+    }
+}
+
+
+var array = [1, 7, 3, 11, 8, 4, 6, 9, 10, 2]
+
+function bubSort(ar) {
+
+    var numOfSwaps = 0;
+console.log(ar)
+    for (var i = 0; i < ar.length; i++) {
+        for (var j = 0; j < ar.length; j++) {
+            if (ar[j] > ar[j + 1]) {
+                [ar[j], ar[j +1]] = [ar[j + 1], ar[j]]
+                numOfSwaps++
+            }
+        }
+    }
+
+console.log(ar, numOfSwaps);
+}
+
+bubSort(array)
